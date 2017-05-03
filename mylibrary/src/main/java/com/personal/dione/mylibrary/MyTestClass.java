3d4a9670b683f76ef8ea7215d0a7e58e67ef5520
Donds <dione@tagcash.com>
2017-05-03 14:49:55 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/personal/dione/mylibrary/MyTestClass.java
@@ -0,0 +1,11 @@
+package com.personal.dione.mylibrary;
+
+/**
+ * Created by Tagcash-321143214324 on 5/3/2017.
+ */
+
+public class MyTestClass {
+    public int TestAddition(int number1, int number2) {
+        return number1 + number2;
+    }
+}
